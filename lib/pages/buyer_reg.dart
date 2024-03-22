2a7db9df8504f6a218993783443fa7da98d53ec1
anisha-n12 <nemadeanisha04@gmail.com>
2024-03-22 17:15:34 +0530

    done

---
+++ lib/pages/buyer_reg.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BuyerRegistration extends StatefulWidget {
+  const BuyerRegistration({super.key});
+
+  @override
+  State<BuyerRegistration> createState() => _BuyerRegistrationState();
+}
+
+class _BuyerRegistrationState extends State<BuyerRegistration> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text("Buyer")));
+  }
+}
---
+++ lib/pages/seller_reg.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SellerRegistration extends StatefulWidget {
+  const SellerRegistration({super.key});
+
+  @override
+  State<SellerRegistration> createState() => _SellerRegistrationState();
+}
+
+class _SellerRegistrationState extends State<SellerRegistration> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Center(child: Text("Seller")));
+  }
+}
