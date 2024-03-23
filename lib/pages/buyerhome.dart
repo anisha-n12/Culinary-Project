d9d643b40cb9d544b1cfe48995e95f29de0e6cd7
anisha-n12 <nemadeanisha04@gmail.com>
2024-03-23 09:58:07 +0530

    cart badge

--- lib/pages/buyerhome.dart
+++ lib/pages/buyerhome.dart
@@ -1,11 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:culinary_project/pages/AddProduct.dart';
+import 'package:culinary_project/pages/ProductDetail.dart';
 import 'package:culinary_project/pages/buyerprofile.dart';
+// import 'package:culinary_project/pages/cart.dart';
 import 'package:culinary_project/pages/homepage.dart';
 import 'package:culinary_project/pages/offergallery.dart';
 import 'package:culinary_project/service/database_service.dart';
 import 'package:culinary_project/shared/constants.dart';
 import 'package:culinary_project/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:badges/badges.dart';
 
 class BuyerHome extends StatelessWidget {
   static const primaryColor = Color.fromARGB(255, 0, 163, 146);
@@ -20,10 +24,15 @@ class BuyerHome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // int itemCount = ShoppingCart.cartItems.le;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         // automaticallyImplyLeading: false,
+        actions: [
+          FavoritesIndicator(),
+          CartIndicator(itemCount: 3),
+        ],
         backgroundColor: Constants.secondaryColor,
         // leading: Image.asset("lib/assets/logosmall.png"),
         title: Column(children: [
@@ -32,9 +41,9 @@ class BuyerHome extends StatelessWidget {
             style: TextStyle(
                 color: Colors.white, fontWeight: FontWeight.bold, fontSize: 30),
           ),
-          SizedBox(height: 5),
+          SizedBox(height: 2),
         ]),
-        toolbarHeight: 80,
+        toolbarHeight: 70,
       ),
       body: SingleChildScrollView(
         child: Column(
@@ -48,9 +57,118 @@ class BuyerHome extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(height: 10),
             Container(
               child: OfferGallery(),
-            )
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Explore",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                future:
+                    DatabaseService().retrieveCollection('productcollection'),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  } else {
+                    List<QueryDocumentSnapshot<Map<String, dynamic>>> products =
+                        snapshot.data!.docs;
+
+                    return GridView.builder(
+                      shrinkWrap: true,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 16.0,
+                        mainAxisSpacing: 16.0,
+                        childAspectRatio: 0.75,
+                      ),
+                      itemCount: products.length,
+                      itemBuilder: (context, index) {
+                        final productData = products[index].data();
+                        final imageUrl = productData['imageurl'] ?? '';
+                        final productName =
+                            productData['productname'] ?? 'Unknown';
+                        final price = productData['price'] is String
+                            ? double.tryParse(productData['price']) ?? 0
+                            : (productData['price'] ?? 0).toDouble();
+                        final productId = products[index].id;
+                        final sellerId = productData['sellerid'];
+
+                        return GestureDetector(
+                          onTap: () async {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => ProductDetail(
+                                  productId: productId,
+                                  sellerId: sellerId,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12.0),
+                              border: Border.all(color: Colors.grey[300]!),
+                            ),
+                            child: Card(
+                              elevation: 0,
+                              margin: EdgeInsets.zero,
+                              clipBehavior: Clip.antiAlias,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(12.0),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: imageUrl.isNotEmpty
+                                        ? Image.network(
+                                            imageUrl,
+                                            fit: BoxFit.cover,
+                                          )
+                                        : Center(child: Text('No Image')),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      productName,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 8.0),
+                                    child: Text('\Rs.${price.toString()}'),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
           ],
         ),
       ),
@@ -202,3 +320,62 @@ class BuyerHome extends StatelessWidget {
     );
   }
 }
+
+class CartIndicator extends StatelessWidget {
+  final int itemCount;
+
+  const CartIndicator({Key? key, required this.itemCount}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        IconButton(
+          icon: Icon(Icons.shopping_cart, size: 35),
+          onPressed: () {
+            // Handle cart button press
+          },
+        ),
+        Positioned(
+          right: 8,
+          top: 8,
+          child: Container(
+            padding: EdgeInsets.all(4),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            constraints: BoxConstraints(
+              minWidth: 5,
+              minHeight: 5,
+            ),
+            child: Text(
+              itemCount.toString(),
+              style: TextStyle(
+                color: Constants.secondaryColor,
+                fontSize: 8,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class FavoritesIndicator extends StatelessWidget {
+  const FavoritesIndicator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.favorite, size: 35),
+      onPressed: () {
+        // Handle favorites button press
+      },
+    );
+  }
+}
+// 
